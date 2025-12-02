90284347f65cbb7ffea474e24542bd297eac0f52
2100030928nallajarla <chaitanya.nallajarla27@gmail.com>
2025-12-02 15:17:15 +0530

    initial commit

---
+++ convert_minutes.py
@@ -0,0 +1,6 @@
+def convert_minutes(m):
+    hrs = m // 60
+    mins = m % 60
+    if hrs == 0: return f"{mins} minutes"
+    elif mins == 0: return f"{hrs} hrs"
+    else: return f"{hrs} hrs {mins} minutes"
